0d768c467ab16c365273e8ab3038997a0816c08a
Kizito Nwose <kizitonwose@gmail.com>
2019-04-20 17:02:37 +0100

    Hide item view TextView if empty.

--- sample/src/main/java/com/kizitonwose/calendarviewsample/ExamplesAdapter.kt
+++ sample/src/main/java/com/kizitonwose/calendarviewsample/ExamplesAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.StringRes
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.options_item_view.*
@@ -11,7 +12,8 @@ import kotlinx.android.synthetic.main.options_item_view.*
 data class ExampleItem(@StringRes val titleRes: Int, @StringRes val subtitleRes: Int, val clazz: Class<*>)
 
 
-class ExamplesAdapter(val onClick: (ExampleItem) -> Unit) : RecyclerView.Adapter<ExamplesAdapter.BaseObjectViewHolder>() {
+class ExamplesAdapter(val onClick: (ExampleItem) -> Unit) :
+    RecyclerView.Adapter<ExamplesAdapter.BaseObjectViewHolder>() {
 
     val examples = mutableListOf<ExampleItem>().apply {
         add(ExampleItem(R.string.example_1, 0, HomeActivity::class.java))
@@ -41,8 +43,12 @@ class ExamplesAdapter(val onClick: (ExampleItem) -> Unit) : RecyclerView.Adapter
 
         fun bind(item: ExampleItem) {
             val context = itemView.context
-            itemOptionTitle.text = context.getString(item.titleRes)
-            itemOptionSubtitle.text = context.getString(item.subtitleRes)
+
+            itemOptionTitle.text = if (item.titleRes != 0) context.getString(item.titleRes) else null
+            itemOptionTitle.isVisible = itemOptionTitle.text.isNotBlank()
+
+            itemOptionSubtitle.text = if (item.subtitleRes != 0) context.getString(item.subtitleRes) else null
+            itemOptionSubtitle.isVisible = itemOptionSubtitle.text.isNotBlank()
         }
     }
 
