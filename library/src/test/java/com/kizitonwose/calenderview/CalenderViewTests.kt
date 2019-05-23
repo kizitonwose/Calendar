4836f07b7aebdbae2c343c24137eb5ca55777e6f
Kizito Nwose <kizitonwose@gmail.com>
2019-05-23 16:25:19 +0100

    More `CalendarMonth` logic tests.

--- library/src/test/java/com/kizitonwose/calenderview/CalenderViewTests.kt
+++ library/src/test/java/com/kizitonwose/calenderview/CalenderViewTests.kt
@@ -18,25 +18,52 @@ class CalenderViewTests {
     private val may2019 = YearMonth.of(2019, 5)
     private val june2019 = YearMonth.of(2019, 6)
 
-    private val endOfRowConfig = CalendarConfig(OutDateStyle.END_OF_ROW, ScrollMode.PAGED, RecyclerView.HORIZONTAL, null)
+    private val endOfRowConfig =
+        CalendarConfig(OutDateStyle.END_OF_ROW, ScrollMode.PAGED, RecyclerView.HORIZONTAL, null)
 
-    private val endOfGridConfig = CalendarConfig(OutDateStyle.END_OF_GRID, ScrollMode.PAGED, RecyclerView.HORIZONTAL, null)
+    private val endOfGridConfig =
+        CalendarConfig(OutDateStyle.END_OF_GRID, ScrollMode.PAGED, RecyclerView.HORIZONTAL, null)
 
 
     @Test
-    fun `test end of row date generation works as expected`() {
+    fun `test in date generation works as expected`() {
         val month = CalendarMonth(may2019, endOfRowConfig, DayOfWeek.MONDAY)
 
-        assertTrue(month.weekDays.flatten().count { it.owner == DayOwner.PREVIOUS_MONTH } == 2) // InDates
-        assertTrue(month.weekDays.flatten().count { it.owner == DayOwner.NEXT_MONTH } == 2) // OutDates
+        val validInDateIndices = (0..1)
+        val inDatesInMonth = month.weekDays.flatten().filterIndexed { index, _ -> validInDateIndices.contains(index) }
+
+        // In dates are in appropriate indices and have accurate count.
+        assertTrue(inDatesInMonth.all { it.owner == DayOwner.PREVIOUS_MONTH })
     }
 
     @Test
-    fun `test end of grid date generation works as expected`() {
+    fun `test end of row out date generation works as expected`() {
+        val month = CalendarMonth(may2019, endOfRowConfig, DayOfWeek.MONDAY)
 
+        val monthIndices = month.weekDays.flatten().indices
+        val validOutDateIndices = monthIndices.toList().takeLast(2)
+        val outDatesInMonth = month.weekDays.flatten().filterIndexed { index, _ -> validOutDateIndices.contains(index) }
+
+        // Out dates are in appropriate indices and have accurate count.
+        assertTrue(outDatesInMonth.all { it.owner == DayOwner.NEXT_MONTH })
+    }
+
+    @Test
+    fun `test end of grid out date generation works as expected`() {
         val month = CalendarMonth(may2019, endOfGridConfig, DayOfWeek.MONDAY)
 
-        assertTrue(month.weekDays.flatten().count { it.owner == DayOwner.PREVIOUS_MONTH } == 2) // InDates
-        assertTrue(month.weekDays.flatten().count { it.owner == DayOwner.NEXT_MONTH } == 9) // OutDates
+        val monthIndices = month.weekDays.flatten().indices
+        val validOutDateIndices = monthIndices.toList().takeLast(9)
+        val outDatesInMonth = month.weekDays.flatten().filterIndexed { index, _ -> validOutDateIndices.contains(index) }
+
+        // Out dates are in appropriate indices and have accurate count.
+        assertTrue(outDatesInMonth.all { it.owner == DayOwner.NEXT_MONTH })
+    }
+
+    @Test
+    fun `test first day of week is in correct position`() {
+        val month = CalendarMonth(may2019, endOfRowConfig, DayOfWeek.MONDAY)
+
+        assertTrue(month.weekDays.first().first().date.dayOfWeek == DayOfWeek.MONDAY)
     }
 }
