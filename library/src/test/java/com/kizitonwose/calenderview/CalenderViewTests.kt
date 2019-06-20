5c043ef3894a5047b536c8dfa6e1df2de2e11751
Kizito Nwose <kizitonwose@gmail.com>
2019-06-20 08:31:48 +0100

    Add more tests.

--- library/src/test/java/com/kizitonwose/calenderview/CalenderViewTests.kt
+++ library/src/test/java/com/kizitonwose/calenderview/CalenderViewTests.kt
@@ -1,6 +1,7 @@
 package com.kizitonwose.calenderview
 
 import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.model.InDateStyle
 import com.kizitonwose.calendarview.model.MonthConfig
 import com.kizitonwose.calendarview.model.OutDateStyle
 import org.junit.Assert.assertTrue
@@ -14,21 +15,42 @@ import org.threeten.bp.YearMonth
  */
 class CalenderViewTests {
 
-    // You can see what May 2019 with Monday as the first
-    // day of week looks like here: https://bit.ly/2HwWZ7o
+    // You can see what May and November 2019 with Monday as the first day of
+    // week look like in the included May2019.png and November2019.png files.
     private val may2019 = YearMonth.of(2019, 5)
     private val nov2019 = may2019.plusMonths(6)
     private val firstDayOfWeek = DayOfWeek.MONDAY
 
     @Test
-    fun `test in date generation works as expected`() {
+    fun `test all month in date generation works as expected`() {
         val weekDays = MonthConfig.generateWeekDays(may2019, firstDayOfWeek, true, OutDateStyle.END_OF_ROW)
 
         val validInDateIndices = (0..1)
         val inDatesInMonth = weekDays.flatten().filterIndexed { index, _ -> validInDateIndices.contains(index) }
 
-        // In dates are in appropriate indices and have accurate count.
+        // inDates are in appropriate indices and have accurate count.
         assertTrue(inDatesInMonth.all { it.owner == DayOwner.PREVIOUS_MONTH })
+    }
+
+    @Test
+    fun `test no in date generation works as expected`() {
+        val weekDays = MonthConfig.generateWeekDays(may2019, firstDayOfWeek, false, OutDateStyle.END_OF_ROW)
+        assertTrue(weekDays.flatten().none { it.owner == DayOwner.PREVIOUS_MONTH })
+    }
+
+    @Test
+    fun `test first month in date generation works as expected`() {
+        val months = MonthConfig.generateBoundedMonths(
+            may2019, nov2019, firstDayOfWeek, 6, InDateStyle.FIRST_MONTH, OutDateStyle.NONE
+        )
+
+        // inDates are in the first month.
+        assertTrue(months.first().weekDays.flatten().any { it.owner == DayOwner.PREVIOUS_MONTH })
+
+        // No inDates in other months.
+        assertTrue(months.takeLast(months.size - 1).all {
+            it.weekDays.flatten().none { it.owner == DayOwner.PREVIOUS_MONTH }
+        })
     }
 
     @Test
@@ -38,7 +60,7 @@ class CalenderViewTests {
         val validOutDateIndices = weekDays.flatten().indices.toList().takeLast(2)
         val outDatesInMonth = weekDays.flatten().filterIndexed { index, _ -> validOutDateIndices.contains(index) }
 
-        // Out dates are in appropriate indices and have accurate count.
+        // outDates are in appropriate indices and have accurate count.
         assertTrue(outDatesInMonth.all { it.owner == DayOwner.NEXT_MONTH })
     }
 
@@ -49,8 +71,14 @@ class CalenderViewTests {
         val validOutDateIndices = weekDays.flatten().indices.toList().takeLast(9)
         val outDatesInMonth = weekDays.flatten().filterIndexed { index, _ -> validOutDateIndices.contains(index) }
 
-        // Out dates are in appropriate indices and have accurate count.
+        // outDates are in appropriate indices and have accurate count.
         assertTrue(outDatesInMonth.all { it.owner == DayOwner.NEXT_MONTH })
+    }
+
+    @Test
+    fun `test no out date generation works as expected`() {
+        val weekDays = MonthConfig.generateWeekDays(may2019, firstDayOfWeek, true, OutDateStyle.NONE)
+        assertTrue(weekDays.flatten().none { it.owner == DayOwner.NEXT_MONTH })
     }
 
     @Test
