29fce296ee020fbadebdb41bd1e311193663627e
Lucas Haug <lucas.haug@pentasys.de>
2020-11-03 18:36:02 +0100

    Fixed for weekmode, added example

--- sample/src/main/java/com/kizitonwose/calendarviewsample/Example1Fragment.kt
+++ sample/src/main/java/com/kizitonwose/calendarviewsample/Example1Fragment.kt
@@ -1,7 +1,6 @@
 package com.kizitonwose.calendarviewsample
 
 import android.animation.ValueAnimator
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
