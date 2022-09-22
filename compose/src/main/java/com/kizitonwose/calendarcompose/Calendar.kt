d9066d42672ac85627d8b489e7fb6f6b4e7c1b0a
Kizito Nwose <kizitonwose@gmail.com>
2022-09-22 09:16:32 +0200

    Prepare for compose support.

---
+++ compose/src/main/java/com/kizitonwose/calendarcompose/Calendar.kt
@@ -0,0 +1,21 @@
+package com.kizitonwose.calendarcompose
+
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+import java.time.DayOfWeek
+import java.time.YearMonth
+
+@Composable
+fun Calendar(
+    modifier: Modifier,
+    startMonth: YearMonth,
+    endMonth: YearMonth,
+    firstDayOfWeek: DayOfWeek
+) {
+    LazyRow {
+
+    }
+}
+
+class CalendarState
