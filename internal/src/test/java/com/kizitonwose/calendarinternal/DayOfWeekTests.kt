df8daa2a5fe7bdad783dc9809c7e866bca3214a9
Kizito Nwose <kizitonwose@gmail.com>
2022-10-06 17:25:18 +0200

    Add tests for calendar date generation.

--- internal/src/test/java/com/kizitonwose/calendarinternal/DayOfWeekTests.kt
+++ internal/src/test/java/com/kizitonwose/calendarinternal/DayOfWeekTests.kt
@@ -4,22 +4,13 @@ import com.kizitonwose.calendarcore.daysOfWeek
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.time.DayOfWeek
-import java.time.YearMonth
 
-class ExtensionTests {
+class DayOfWeekTests {
     @Test
     fun `days until works as expected`() {
         assertEquals(5, DayOfWeek.FRIDAY.daysUntil(DayOfWeek.WEDNESDAY))
         assertEquals(2, DayOfWeek.TUESDAY.daysUntil(DayOfWeek.THURSDAY))
         assertEquals(0, DayOfWeek.SUNDAY.daysUntil(DayOfWeek.SUNDAY))
-    }
-
-    @Test
-    fun `month count includes start index`() {
-        val startMonth = YearMonth.now()
-        assertEquals(1, getMonthIndicesCount(startMonth, startMonth))
-        assertEquals(3, getMonthIndicesCount(startMonth,
-            startMonth.plusMonths(2)))
     }
 
     @Test
