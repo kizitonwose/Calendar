731add9c9406e0220e04676f010705d08f676cf5
Kizito Nwose <kizitonwose@gmail.com>
2024-02-10 21:59:08 +0100

    Prepare version 2.5.0

--- buildSrc/src/main/java/com/kizitonwose/calendar/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/com/kizitonwose/calendar/buildsrc/Dependencies.kt
@@ -1,4 +1,4 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "ConstPropertyName")
 
 package com.kizitonwose.calendar.buildsrc
 
@@ -18,26 +18,26 @@ object Android {
 
     // See compose/kotlin version mapping
     // https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-    const val composeCompiler = "1.5.3"
+    const val composeCompiler = "1.5.9"
 }
 
 object Plugins {
     const val android = "com.android.tools.build:gradle:8.1.0"
     const val kotlin = Kotlin.gradlePlugin
-    const val kotlinter = "org.jmailen.gradle:kotlinter-gradle:3.16.0"
-    const val versions = "com.github.ben-manes:gradle-versions-plugin:0.48.0"
-    const val mavenPublish = "com.vanniktech:gradle-maven-publish-plugin:0.25.3"
+    const val kotlinter = "org.jmailen.gradle:kotlinter-gradle:4.2.0"
+    const val versions = "com.github.ben-manes:gradle-versions-plugin:0.51.0"
+    const val mavenPublish = "com.vanniktech:gradle-maven-publish-plugin:0.27.0"
 }
 
 object Kotlin {
-    private const val version = "1.9.10"
+    private const val version = "1.9.22"
     const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
     const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
 }
 
 object Libs {
     object Core {
-        const val deSugar = "com.android.tools:desugar_jdk_libs:2.0.3"
+        const val deSugar = "com.android.tools:desugar_jdk_libs:2.0.4"
 
         object Test {
             const val junit = "junit:junit:4.13.2"
@@ -50,7 +50,7 @@ object Libs {
         const val coreKtx = "androidx.core:core-ktx:1.12.0"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.1.4"
         const val cardView = "androidx.cardview:cardview:1.0.0"
-        const val recyclerView = "androidx.recyclerview:recyclerview:1.3.1"
+        const val recyclerView = "androidx.recyclerview:recyclerview:1.3.2"
         const val material = "com.google.android.material:material:1.9.0"
 
         object Test {
@@ -64,14 +64,14 @@ object Libs {
     }
 
     object Compose {
-        private const val composeVersion = "1.6.0-beta02"
+        private const val composeVersion = "1.6.1"
         const val ui = "androidx.compose.ui:ui:$composeVersion"
         const val foundation = "androidx.compose.foundation:foundation:$composeVersion"
         const val tooling = "androidx.compose.ui:ui-tooling:$composeVersion"
         const val runtime = "androidx.compose.runtime:runtime:$composeVersion"
         const val material = "androidx.compose.material:material:$composeVersion"
-        const val activity = "androidx.activity:activity-compose:1.7.2"
-        const val navigation = "androidx.navigation:navigation-compose:2.7.2"
+        const val activity = "androidx.activity:activity-compose:1.8.2"
+        const val navigation = "androidx.navigation:navigation-compose:2.7.7"
 
         object Test {
             const val uiJunit = "androidx.compose.ui:ui-test-junit4:$composeVersion"
