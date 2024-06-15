ef06e946ab6e0115099542980a89fb303a5a9455
Kizito Nwose <kizitonwose@gmail.com>
2024-06-15 21:58:22 +0200

    Annotate data classes.

--- compose/src/main/java/com/kizitonwose/calendar/compose/CalendarInfo.kt
+++ compose/src/main/java/com/kizitonwose/calendar/compose/CalendarInfo.kt
@@ -1,8 +1,10 @@
 package com.kizitonwose.calendar.compose
 
+import androidx.compose.runtime.Immutable
 import com.kizitonwose.calendar.core.OutDateStyle
 import java.time.DayOfWeek
 
+@Immutable
 internal data class CalendarInfo(
     val indexCount: Int,
     private val firstDayOfWeek: DayOfWeek? = null,
--- compose/src/main/java/com/kizitonwose/calendar/compose/VisibleItemState.kt
+++ compose/src/main/java/com/kizitonwose/calendar/compose/VisibleItemState.kt
@@ -1,7 +1,9 @@
 package com.kizitonwose.calendar.compose
 
+import androidx.compose.runtime.Immutable
 import java.io.Serializable
 
+@Immutable
 internal class VisibleItemState(
     val firstVisibleItemIndex: Int = 0,
     val firstVisibleItemScrollOffset: Int = 0,
