85324b87095fc0a96baf970e3afbb97eaf7e5f81
Goooler <wangzongler@gmail.com>
2024-07-24 11:46:52 +0800

    Enable KLib ABI validation
    https://github.com/Kotlin/binary-compatibility-validator?tab=readme-ov-file#experimental-klib-abi-validation-support

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,11 @@ apiValidation {
     ignoredProjects += listOf(
         "sample",
     )
+
+    @OptIn(kotlinx.validation.ExperimentalBCVApi::class)
+    klib {
+        enabled = true
+    }
 }
 
 // tasks.register<Delete>("clean").configure {
