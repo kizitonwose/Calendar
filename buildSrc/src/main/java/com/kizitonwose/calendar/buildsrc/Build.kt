c2169352645d479ca3c442a50595e72d367a8cb6
Kizito Nwose <kizitonwose@gmail.com>
2024-07-27 09:55:51 +0200

    Prepare next development version.

--- buildSrc/src/main/java/com/kizitonwose/calendar/buildsrc/Build.kt
+++ buildSrc/src/main/java/com/kizitonwose/calendar/buildsrc/Build.kt
@@ -12,8 +12,8 @@ object Config {
 }
 
 object Version {
-    val android = "2.6.0-beta03"
-    val multiplatfrom = "2.6.0-alpha04"
+    val android = "2.6.0-SNAPSHOT"
+    val multiplatfrom = "2.6.0-SNAPSHOT"
 
     fun String.isNoPublish() = this == VERSION_NO_PUBLISH
 }
