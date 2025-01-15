5d16b6454220600910107b62e6a4e55e9ab0ef53
Kizito Nwose <kizitonwose@gmail.com>
2025-01-15 22:41:13 +0100

    scroll to day logic

--- data/src/main/java/com/kizitonwose/calendar/data/Extensions.kt
+++ data/src/main/java/com/kizitonwose/calendar/data/Extensions.kt
@@ -1,6 +1,25 @@
 package com.kizitonwose.calendar.data
 
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.nextMonth
+import com.kizitonwose.calendar.core.previousMonth
+import com.kizitonwose.calendar.core.yearMonth
 import java.time.DayOfWeek
+import java.time.YearMonth
 
 // E.g DayOfWeek.SATURDAY.daysUntil(DayOfWeek.TUESDAY) = 3
 public fun DayOfWeek.daysUntil(other: DayOfWeek): Int = (7 + (other.ordinal - ordinal)) % 7
+
+// Find the actual month on the calendar where this date is shown.
+public val CalendarDay.positionYearMonth: YearMonth
+    get() = when (position) {
+        DayPosition.InDate -> date.yearMonth.nextMonth
+        DayPosition.MonthDate -> date.yearMonth
+        DayPosition.OutDate -> date.yearMonth.previousMonth
+    }
+
+public inline fun <T> Iterable<T>.indexOfFirstOrNull(predicate: (T) -> Boolean): Int? {
+    val result = indexOfFirst(predicate)
+    return if (result == -1) null else result
+}
--- view/src/main/java/com/kizitonwose/calendar/view/internal/Extensions.kt
+++ view/src/main/java/com/kizitonwose/calendar/view/internal/Extensions.kt
@@ -5,26 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
-import com.kizitonwose.calendar.core.CalendarDay
-import com.kizitonwose.calendar.core.DayPosition
-import com.kizitonwose.calendar.core.nextMonth
-import com.kizitonwose.calendar.core.previousMonth
-import com.kizitonwose.calendar.core.yearMonth
-import java.time.YearMonth
 
 internal fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
 }
 
 internal const val NO_INDEX = -1
-
-// Find the actual month on the calendar where this date is shown.
-internal val CalendarDay.positionYearMonth: YearMonth
-    get() = when (position) {
-        DayPosition.InDate -> date.yearMonth.nextMonth
-        DayPosition.MonthDate -> date.yearMonth
-        DayPosition.OutDate -> date.yearMonth.previousMonth
-    }
 
 internal fun Rect.intersects(other: Rect): Boolean {
     return if (this.isEmpty || other.isEmpty) {
